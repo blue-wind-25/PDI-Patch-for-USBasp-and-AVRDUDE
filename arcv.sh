fbc8ae276d4306ebd2e3a1b3bbfc42206c4d5acf
Aloysius Indrayanto <aloysius@devel.centos7>
2023-07-16 06:41:29 +0700

    Bug fix.

--- arcv.sh
+++ arcv.sh
@@ -1,17 +1,22 @@
 COPY_NAME=PDI-Patch-for-USBasp-and-AVRDUDE-GIT-`date +'%Y%m%d-%H%M'`
 
 echo -e "Cleaning the project tree ..."
-cd ..
+rm -rf usbasp.2011-05-28/work/*
+rm -rf avrdude-7.1/work/*
 
 echo -e "Copying the project tree ..."
 cd ..
-cp -Rv PDI-Patch-for-USBasp-and-AVRDUDE $$COPY_NAME > /dev/null
+cp -Rv PDI-Patch-for-USBasp-and-AVRDUDE ${COPY_NAME} > /dev/null
 
 echo -e "Deleting the '.git' directory from the copied project tree ..."
-cd $$COPY_NAME
+cd ${COPY_NAME}
 rm -rvf .git > /dev/null
 
 echo -e "Archiving files from the copied project tree ..."
 cd ..
-tar -cjvpf $${COPY_NAME}.tar.bz2 $$COPY_NAME > /dev/null
-echo -e "Done '$${COPY_NAME}.tar.bz2'"
+tar -cjvpf ${COPY_NAME}.tar.bz2 ${COPY_NAME} > /dev/null
+
+echo -e "Deleting the copied project tree ..."
+rm -rf ${COPY_NAME}
+
+echo -e "Done '${COPY_NAME}.tar.bz2'"
